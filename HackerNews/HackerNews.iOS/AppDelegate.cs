42148b32b1006120cc189318935a08b378d49677
brminnick <brandon.r.minnick@gmail.com>
2018-01-25 14:59:44 -0800

    Renamed iOS & Droid folders

--- HackerNews/HackerNews.iOS/AppDelegate.cs
+++ HackerNews/HackerNews.iOS/AppDelegate.cs
@@ -6,13 +6,13 @@ namespace HackerNews.iOS
     [Register(nameof(AppDelegate))]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
         {
             global::Xamarin.Forms.Forms.Init();
 
             LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
+            return base.FinishedLaunching(uiApplication, launchOptions);
         }
     }
 }
