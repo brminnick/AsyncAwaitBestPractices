32694092f6eedde9973c956b97ecbc820dc24fb6
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-29 11:17:06 -0700

    Added ConfigureAwait(false)

--- HackerNews/HackerNews/ViewModels/NewsViewModel_GoodAsyncAwaitPractices.cs
+++ HackerNews/HackerNews/ViewModels/NewsViewModel_GoodAsyncAwaitPractices.cs
@@ -7,79 +7,76 @@ using Xamarin.Forms;
 
 namespace HackerNews
 {
-    public class NewsViewModel_GoodAsyncAwaitPractices : BaseViewModel
-    {
-        #region Fields
-        bool _isListRefreshing;
-        ICommand _refreshCommand;
-        List<StoryModel> _topStoryList;
-        #endregion
+	public class NewsViewModel_GoodAsyncAwaitPractices : BaseViewModel
+	{
+		#region Fields
+		bool _isListRefreshing;
+		ICommand _refreshCommand;
+		List<StoryModel> _topStoryList;
+		#endregion
 
-        #region Constructors
-        public NewsViewModel_GoodAsyncAwaitPractices()
-        {
-            RefreshCommand?.Execute(null);
-        }
-        #endregion
+		#region Constructors
+		public NewsViewModel_GoodAsyncAwaitPractices() => RefreshCommand?.Execute(null);
+		#endregion
 
-        #region Properties
-        public ICommand RefreshCommand => _refreshCommand ?? 
-            (_refreshCommand = new Command(async () => await ExecuteRefreshCommand()));
+		#region Properties
+		public ICommand RefreshCommand => _refreshCommand ??
+			(_refreshCommand = new Command(async () => await ExecuteRefreshCommand().ConfigureAwait(false)));
 
-        public List<StoryModel> TopStoryList
-        {
-            get => _topStoryList;
-            set => SetProperty(ref _topStoryList, value);
-        }
+		public List<StoryModel> TopStoryList
+		{
+			get => _topStoryList;
+			set => SetProperty(ref _topStoryList, value);
+		}
 
-        public bool IsListRefreshing
-        {
-            get => _isListRefreshing;
-            set => SetProperty(ref _isListRefreshing, value);
-        }
-        #endregion
+		public bool IsListRefreshing
+		{
+			get => _isListRefreshing;
+			set => SetProperty(ref _isListRefreshing, value);
+		}
+		#endregion
 
-        #region Methods
-        async Task ExecuteRefreshCommand()
-        {
-            IsListRefreshing = true;
+		#region Methods
+		async Task ExecuteRefreshCommand()
+		{
+			IsListRefreshing = true;
 
-            try
-            {
-                TopStoryList = await GetTopStories(20).ConfigureAwait(false);
-            }
-            finally
-            {
-                IsListRefreshing = false;
-            }
-        }
+			try
+			{
+				TopStoryList = await GetTopStories(20).ConfigureAwait(false);
+			}
+			finally
+			{
+				IsListRefreshing = false;
+			}
+		}
 
-        async Task<List<StoryModel>> GetTopStories(int numberOfStories)
-        {
-            var topStoryIds = await GetTopStoryIDs().ConfigureAwait(false);
+		async Task<List<StoryModel>> GetTopStories(int numberOfStories)
+		{
+			var topStoryIds = await GetTopStoryIDs().ConfigureAwait(false);
 
-            var getTop20StoriesTaskList = new List<Task<StoryModel>>();
-            for (int i = 0; i < numberOfStories; i++)
-            {
-                getTop20StoriesTaskList.Add(GetStory(topStoryIds[i]));
-            }
+			var getTop20StoriesTaskList = new List<Task<StoryModel>>();
+			for (int i = 0; i < numberOfStories; i++)
+			{
+				getTop20StoriesTaskList.Add(GetStory(topStoryIds[i]));
+			}
 
-            await Task.WhenAll(getTop20StoriesTaskList).ConfigureAwait(false);
+			await Task.WhenAll(getTop20StoriesTaskList).ConfigureAwait(false);
 
-            var topStoryList = new List<StoryModel>();
-            foreach (var getStoryTask in getTop20StoriesTaskList)
-            {
-                topStoryList.Add(await getStoryTask.ConfigureAwait(false));
-            }
+			var topStoryList = new List<StoryModel>();
+			foreach (var getStoryTask in getTop20StoriesTaskList)
+			{
+				topStoryList.Add(await getStoryTask.ConfigureAwait(false));
+			}
 
-            return topStoryList.OrderByDescending(x => x.Score).ToList();
-        }
+			return topStoryList.OrderByDescending(x => x.Score).ToList();
+		}
 
-        Task<List<string>> GetTopStoryIDs() =>
-            GetDataObjectFromAPI<List<string>>("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+		Task<List<string>> GetTopStoryIDs() =>
+			GetDataObjectFromAPI<List<string>>("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
 
-        Task<StoryModel> GetStory(string storyId) =>
-            GetDataObjectFromAPI<StoryModel>($"https://hacker-news.firebaseio.com/v0/item/{storyId}.json?print=pretty");
-        #endregion
-    }
+		Task<StoryModel> GetStory(string storyId) =>
+			GetDataObjectFromAPI<StoryModel>($"https://hacker-news.firebaseio.com/v0/item/{storyId}.json?print=pretty");
+		#endregion
+	}
 }
