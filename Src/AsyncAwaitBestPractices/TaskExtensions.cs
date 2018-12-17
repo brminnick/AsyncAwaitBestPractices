d7a977308e7e545e501e2d026b159119ac5f479f
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2018-12-17 10:46:35 -0800

    Added completeAsynchronously
    This forces the Task parameter in SafeFireAndForget to a backgroun thread

--- Src/AsyncAwaitBestPractices/TaskExtensions.cs
+++ Src/AsyncAwaitBestPractices/TaskExtensions.cs
@@ -12,11 +12,14 @@
         /// <param name="continueOnCapturedContext">If set to <c>true</c> continue on captured context; this will ensure that the Synchronization Context returns to the calling thread. If set to <c>false</c> continue on a different context; this will allow the Synchronization Context to continue on a different thread</param>
         /// <param name="onException">If an exception is thrown in the Task, <c>onException</c> will execute. If onException is null, the exception will be re-thrown</param>
 #pragma warning disable RECS0165 // Asynchronous methods should return a Task instead of void
-        public static async void SafeFireAndForget(this System.Threading.Tasks.Task task, bool continueOnCapturedContext = true, System.Action<System.Exception> onException = null)
+        public static async void SafeFireAndForget(this System.Threading.Tasks.Task task, bool continueOnCapturedContext = false, System.Action<System.Exception> onException = null, bool completeAsynchronously = true)
 #pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
         {
             try
             {
+                if (completeAsynchronously)
+                    await System.Threading.Tasks.Task.Yield();
+
                 await task.ConfigureAwait(continueOnCapturedContext);
             }
             catch (System.Exception ex)
