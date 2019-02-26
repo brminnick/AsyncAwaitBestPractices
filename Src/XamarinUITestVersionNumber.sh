d1bc0999eefa6e1817430a9d979b87e46311d592
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-02-26 10:47:13 -0800

    Added UITestBuildDir

--- Src/XamarinUITestVersionNumber.sh
+++ Src/XamarinUITestVersionNumber.sh
@@ -5,6 +5,14 @@ echo UITestProject: $UITestProject
 UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,10\}\-'dev`
 echo UITestPrereleaseVersionNumber: $UITestVersionNumber
 
+UITestDLL=`find "D:\a\1\s"  -name "HackerNews.UITests.dll" | grep bin`
+echo UITestDLL: $UITestDLL
+
+UITestBuildDir=`dirname $UITestDLL`
+echo UITestBuildDir: $UITestBuildDir
+
+##vso[task.setvariable variable=UITestBuildDir;isSecret=false;isOutput=true;]$UITestBuildDir
+
 UITestVersionNumberSize=${#UITestVersionNumber} 
 echo UITestVersionNumberSize: $UITestVersionNumberSize
 
