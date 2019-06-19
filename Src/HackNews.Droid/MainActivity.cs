d98c69f04a2a936fc535997660451d41de6a7a64
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-06-19 07:48:32 +0200

    Implement Xamarin.Essentials.Browser

--- Src/HackNews.Droid/MainActivity.cs
+++ Src/HackNews.Droid/MainActivity.cs
@@ -1,12 +1,19 @@
-﻿using Android.OS;
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
 
 namespace HackerNews.Droid
 {
     [Activity(Label = "HackerNews.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -14,13 +21,13 @@ namespace HackerNews.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState); 
 
             LoadApplication(new App());
         }
 
-        [Android.Runtime.Preserve, Java.Interop.Export(nameof(GetStoriesAsBase64String))]
+        [Preserve, Java.Interop.Export(nameof(GetStoriesAsBase64String))]
         public string GetStoriesAsBase64String() => BackdoorMethodServices.GetStoriesAsBase64String();
     }
 }
